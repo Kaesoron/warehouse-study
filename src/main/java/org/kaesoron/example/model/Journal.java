4f90e5d76e785edf96296409fbd03deb8c6d24b9
Kaesoron <postal0055@gmail.com>
2023-05-08 17:47:17 +0300

    Final creating models

--- src/main/java/org/kaesoron/example/model/Journal.java
+++ src/main/java/org/kaesoron/example/model/Journal.java
@@ -1,4 +1,10 @@
 package org.kaesoron.example.model;
 
+import java.util.Date;
+
 public class Journal {
+    private long operationId;
+    private OperationType operationType;
+    private Date operationTime;
+
 }
---
+++ src/main/java/org/kaesoron/example/model/OperationType.java
@@ -0,0 +1,6 @@
+package org.kaesoron.example.model;
+
+public enum OperationType {
+    INCOMING,
+    OUTCOMING
+}
